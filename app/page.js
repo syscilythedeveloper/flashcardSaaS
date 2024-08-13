5322a852e294598dc3569dded48d1547960780c6
Big Sys <Owner@macbook-air.myfiosgateway.com>
2024-08-12 22:08:50 -0400

    api, sign-in, firebase, middleware

--- app/page.js
+++ app/page.js
@@ -1,95 +1,130 @@
 import Image from "next/image";
-import styles from "./page.module.css";
+import getStripe from "@/utils/get-stripe";
+import {SignedIn, SignedOut, UserButton} from "@clerk/nextjs"
+import { Typography, Container, AppBar, Grid, Toolbar, Button, Box} from "@mui/material";
+import Head from "next/head";
+
+
 
 export default function Home() {
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>app/page.js</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
+    <Container maxWidth="lg">
+      <Head>
+        <title>Flashcard SaaS</title>
+        <meta name = "description" content = "Create flashcard from your text" />
+      </Head>
 
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore starter templates for Next.js.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+      <AppBar position="static">
+        <Toolbar>
+          <Typography variant="h6" style={{flexGrow:1}}>
+            Flashcard Sass
+            </Typography>
+        
+        <SignedOut>
+          <Button color="inherit"> Login</Button>
+          <Button color="inherit"> Signup</Button>
+        </SignedOut>
+        <SignedIn>
+          <UserButton />
+        </SignedIn>
+        </Toolbar>
+      </AppBar>
+      <Box
+      sx={{textAlign:"center",
+        my:4,
+      }}
+      
+      >
+        <Typography variant ="h2" gutterBottom>Welcome to Flashcard SaaS</Typography>
+        <Typography variant ="h5">
+          {" "}
+          The easiest way to make flashcards from your text</Typography>
+          <Button variant="contained" color="primary" sx={{mt:2}}>
+            Get Started
+          </Button>
+      </Box>
+      <Box sx={{
+        my:6
+      }}>
+        <Typography variant ="h4" components = "h2" gutterBottom> Features</Typography>
+        <Grid container spacing ={4}>
+          <Grid item xs={12} md={4}>
+            <Typography variant="h6" gutterBottom>
+              Easy Text Input
+            </Typography>
+            <Typography>
+              {" "}
+              Simply input your text adn let our software do the rest. Creating flashCards has never been easier. 
+            </Typography>
+          </Grid>
+          <Grid item xs={12} md={4}>
+            <Typography variant="h6" gutterBottom>
+              Smart FlashCards 
+            </Typography>
+            <Typography>
+            {" "}
+              Our AI intelligently breaks down your text into concise flashcards, perfect for stuying.  
+            </Typography>
+          </Grid>
+          <Grid item xs={12} md={4}>
+            <Typography variant="h6">
+              Accsible anywhere 
+            </Typography>
+            <Typography>
+            {" "}
+              Aces your flashcards from any device at any time. Study on the go
+            </Typography>
+          </Grid>
+        </Grid>
+      </Box>
+      <Box sx={{my:6, textAlign:"center" }}>
+      <Typography variant="h4" gutterBottom>Pricing</Typography>
+      <Grid container spacing ={4}>
+          <Grid item xs={12} md={6}>
+            <Box sx={{
+              p: 3,
+              border: "1px solid",
+              borderColor: "grey.300",
+              borderRadius: 2,
+            }}>
+            <Typography variant="h5" gutterBottom>
+              Basic
+            </Typography>
+            <Typography variant="h6" gutterBottom>
+              $5/Month
+            </Typography>
+            <Typography>
+              {" "}
+              Access to basic flashcard features and limited storage
+            </Typography>
+            <Button variant = "contained"color="primary" sx={{mt:2}}> Choose Basic</Button>
+            </Box>
+          </Grid>
+          <Grid item xs={12} md={6}>
+            <Box sx={{
+              p: 3,
+              border: "1px solid",
+              borderColor: "grey.300",
+              borderRadius: 2,
+            }}>
+            <Typography variant="h5" gutterBottom>
+              Pro
+            </Typography>
+            <Typography variant="h6" gutterBottom>
+              $10/Month
+            </Typography>
+            <Typography>
+              {" "}
+              Unlimited flashcards and storage, with priority support.
+            </Typography>
+            <Button variant = "contained"color="primary" sx={{mt:2}}> Choose Pro</Button>
+            </Box>
+          </Grid>
+         
+       
+        </Grid>
+        </Box>
+    </Container>
+    
   );
 }
---
+++ firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAKoogkyT6Igb259hgWWLUvkx2xkmaMsH4",
+  authDomain: "flashcardsass-3b685.firebaseapp.com",
+  projectId: "flashcardsass-3b685",
+  storageBucket: "flashcardsass-3b685.appspot.com",
+  messagingSenderId: "7808030413",
+  appId: "1:7808030413:web:d4ed68285e789b27d87b84"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
