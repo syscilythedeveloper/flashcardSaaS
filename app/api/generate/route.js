c2eb46d9d60ef5b9dcf3c1ce37f32b4e624e93c2
Syscily <Owner@macbook-air.myfiosgateway.com>
2024-08-14 14:38:17 -0400

    adjusting systemPrompt

--- app/api/generate/route.js
+++ app/api/generate/route.js
@@ -2,7 +2,7 @@ import { NextResponse } from "next/server";
 import OpenAI from "openai";
 
 const openai = new OpenAI({
-    apiKey: process.env.OPENAI_API_KEY, // Ensure the API key is set correctly
+    apiKey: process.env.OPENAI_API_KEY,
 });
 
 const systemPrompt = `You are a flashcard creator.
@@ -10,7 +10,7 @@ Your task is to help the user generate flashcards for studying.
 These flashcards should be clear, concise, and cover key points of the subject matter. 
 Provide both questions and answers, and ensure that the content is accurate and well-organized.
 Automatically generate flashcards based on the provided input, ensuring each flashcard has a question on one side and an answer on the other.
-Only generate 12 flashcards.
+Always generate 12 flashcards.
 Always return your response in the following strict JSON format without any additional text or commentary:
 { 
   "flashcards" : [{
